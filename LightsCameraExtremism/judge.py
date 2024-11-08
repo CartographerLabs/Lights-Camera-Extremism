225588f665ba00537f32bffbaec3d3be8d39fb5b
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-11-08 14:22:38 +0000

    Update judge.py

--- LightsCameraExtremism/judge.py
+++ LightsCameraExtremism/judge.py
@@ -5,13 +5,13 @@ import json
 
 class Judge(Agent):
     def enforce(self, enforcement, input):
-        prompt = (f"You are an expert social scientist. Your job is to review text and assess if it was written by an AI large language model. You need to ensure that the text was written by a human."
+        prompt = (f"You are an expert social scientist. Your job is to review text and assess if it was written by an AI large language model."
                   f"Text: '{input}'."
                   f"Return your response in raw json, no surrounding text."
                   )
 
 
-        schema = {"RESULT":"A boolean representation of True if the text is AI generated or false if not.",
+        schema = {"RESULT":"A boolean representation of True if the text is AI generated or False if not.",
                   "FEEDBACK":"Text on what could be improved to make the text meet the criteria."}
 
         schema = json.dumps(schema)
