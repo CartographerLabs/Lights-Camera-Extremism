71aa13a0e1f46f3292bf1c3d488a34f0cb9f3359
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-11-08 20:10:07 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='LightsCameraExtremism',
-    version='0.1.36',
+    version='0.1.37',
     description='A simulation of social network interactions using language models.',
     packages=find_packages(),
     install_requires=requirements,
